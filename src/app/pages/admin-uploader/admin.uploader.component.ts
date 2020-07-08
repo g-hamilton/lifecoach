71afab818b664b87c50c2651b5acaf761ea91ab1
Greg Hamilton <greghamilton@Gregs-MacBook-Pro.local>
2020-07-08 20:14:36 +0100

    admin uploader for storing platform content

--- src/app/pages/admin-uploader/admin.uploader.component.ts
+++ src/app/pages/admin-uploader/admin.uploader.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { AuthService } from 'app/services/auth.service';
 
 @Component({
   selector: 'app-coach-services',
@@ -8,15 +9,22 @@ import { isPlatformBrowser } from '@angular/common';
 export class AdminUploaderComponent implements OnInit {
 
   public browser: boolean;
+  public userId: string;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
 
     if (isPlatformBrowser(this.platformId)) {
-        this.browser = true;
+      this.browser = true;
+      this.authService.getAuthUser().subscribe(user => {
+        if (user) {
+          this.userId = user.uid;
+        }
+      });
     }
 
   }
