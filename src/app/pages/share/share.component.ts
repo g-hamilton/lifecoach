286bb16a09d5dd81aaac06bd58330da6a2af51c8
Volodymyr <4@cravter.com>
2020-07-28 14:57:10 +0300

    subscriptions refactoring

--- src/app/pages/share/share.component.ts
+++ src/app/pages/share/share.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, OnDestroy } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 import { AuthService } from '../../services/auth.service';
@@ -7,69 +7,77 @@ import { AnalyticsService } from '../../services/analytics.service';
 
 import { CoachProfile } from 'app/interfaces/coach.profile.interface';
 import { CloudFunctionsService } from 'app/services/cloud-functions.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-share',
-    templateUrl: 'share.component.html'
+  selector: 'app-share',
+  templateUrl: 'share.component.html'
 })
-export class ShareComponent implements OnInit {
+export class ShareComponent implements OnInit, OnDestroy {
 
-    public browser: boolean;
-    private userId: string;
-    public profile: CoachProfile;
-    public generatingSmartlink: boolean;
+  public browser: boolean;
+  private userId: string;
+  public profile: CoachProfile;
+  public generatingSmartlink: boolean;
+  private subscriptions: Subscription = new Subscription();
 
-    constructor(
-        @Inject(PLATFORM_ID) private platformId: object,
-        private authService: AuthService,
-        private dataService: DataService,
-        private analyticsService: AnalyticsService,
-        private cloudFunctionsService: CloudFunctionsService
-    ) { }
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    private authService: AuthService,
+    private dataService: DataService,
+    private analyticsService: AnalyticsService,
+    private cloudFunctionsService: CloudFunctionsService
+  ) {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.browser = true;
+      this.analyticsService.pageView();
 
-        if (isPlatformBrowser(this.platformId)) {
-
-            this.browser = true;
-
-            this.analyticsService.pageView();
-
-            // Get user auth data
-            this.authService.getAuthUser()
-            .subscribe(user => {
-                if (user) {
-                    this.userId = user.uid;
-                    this.dataService.getCoachProfile(this.userId).subscribe(profile => {
-                        if (profile) { // a coach profile with shortUrl exists for this user
-                            this.profile = profile;
-                        }
-                    });
-                }
-            });
-        }
-    }
-
-    async generateSmartlink() {
-        if (!this.generatingSmartlink) {
-            this.generatingSmartlink = true;
-
-            if (!this.profile.profileUrl) { // catch the case where profileUrl is not defined
-                this.profile.profileUrl = `https://lifecoach.io/coach/${this.userId}`;
-                this.dataService.saveCoachProfile(this.userId, this.profile);
+      // Get user auth data
+      this.subscriptions.add(
+        this.authService.getAuthUser()
+          .subscribe(user => {
+            if (user) {
+              this.userId = user.uid;
+              this.subscriptions.add(
+                this.dataService.getCoachProfile(this.userId).subscribe(profile => {
+                  if (profile) { // a coach profile with shortUrl exists for this user
+                    this.profile = profile;
+                  }
+                })
+              );
             }
-
-            const res = await this.cloudFunctionsService.generateShortUrl(this.userId, this.profile.profileUrl) as any;
-
-            if (!res.error) {
-                // success
-                console.log(res);
-            } else {
-                // alert res.error.
-            }
-
-            this.generatingSmartlink = false;
-        }
+          })
+      );
     }
+  }
+
+  async generateSmartlink() {
+    if (!this.generatingSmartlink) {
+      this.generatingSmartlink = true;
+
+      if (!this.profile.profileUrl) { // catch the case where profileUrl is not defined
+        this.profile.profileUrl = `https://lifecoach.io/coach/${this.userId}`;
+        this.dataService.saveCoachProfile(this.userId, this.profile);
+      }
+
+      const res = await this.cloudFunctionsService.generateShortUrl(this.userId, this.profile.profileUrl) as any;
+
+      if (!res.error) {
+        // success
+        console.log(res);
+      } else {
+        // alert res.error.
+      }
+
+      this.generatingSmartlink = false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
 }
