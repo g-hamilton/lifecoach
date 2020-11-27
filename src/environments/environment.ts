08d402fdd312beb8f679a69207935c9d4a0fbce1
Alex <alexandrit98226@gmail.com>
2020-11-27 14:06:32 +0200

    fixed algolia

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyADUNrRkAQSclYoRhi_9y-sb1FDEHCmuE8',
+  apiKey: '6d1a3f4cbc6d1b0a835f6acf4c868348',
   authDomain: 'livecoach-dev.firebaseapp.com',
   databaseURL: 'https://livecoach-dev.firebaseio.com',
   projectId: 'livecoach-dev',
@@ -13,7 +13,7 @@ export const environment = {
   appId: '1:1000559055215:web:33ebbc33ff1f23c203189a',
   measurementId: 'G-S4801FLL5M',
   algoliaApiKey: '2fb04a22ac2afb05afcf086bbf5289e3',
-  algoliaApplicationID: 'T46GDX1NCS',
+  algoliaApplicationID: 'PXC7SZHHT9',
   twilioAccountSID: 'AC7cea98ce8762206b3f5af09b63e9ebbf', // There are test credentials. Need to be changed for prod
   twilioAuthToken: 'c9775bfe4f0fd11c71b935dfb45f81c7'
 };
