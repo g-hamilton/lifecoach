929792fbd34f0c78831b59dfac3833af3be5650c
Greg Hamilton <greghamilton@Gregs-MacBook-Pro.local>
2020-12-05 18:51:47 +0000

    bug fix to program submit

--- src/app/components/program-submit/program-submit.component.ts
+++ src/app/components/program-submit/program-submit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, OnInit, Input, Inject, PLATFORM_ID, OnChanges } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { DataService } from 'app/services/data.service';
 import { AlertService } from 'app/services/alert.service';
 import { AnalyticsService } from 'app/services/analytics.service';
@@ -10,11 +11,12 @@ import { CoachingProgram } from 'app/interfaces/coach.program.interface';
   templateUrl: './program-submit.component.html',
   styleUrls: ['./program-submit.component.scss']
 })
-export class ProgramSubmitComponent implements OnInit {
+export class ProgramSubmitComponent implements OnInit, OnChanges {
 
   @Input() userId: string;
   @Input() program: CoachingProgram;
 
+  public programForm: FormGroup;
   public browser: boolean;
   public requesting: boolean;
 
@@ -22,13 +24,79 @@ export class ProgramSubmitComponent implements OnInit {
     @Inject(PLATFORM_ID) public platformId: object,
     private dataService: DataService,
     private alertService: AlertService,
-    private analyticsService: AnalyticsService
+    private analyticsService: AnalyticsService,
+    public formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       this.browser = true;
+      this.buildProgramForm(); // only building for client-side submission ready validation
     }
+  }
+
+  ngOnChanges() {
+    if (this.program) {
+      this.importProgramData();
+    }
+  }
+
+  buildProgramForm() {
+    this.programForm = this.formBuilder.group({
+      programId: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      subtitle: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      language: [null, [Validators.required]],
+      category: [null, [Validators.required]],
+      level: [null, [Validators.required]],
+      subject: ['', [Validators.required]],
+      pricingStrategy: ['flexible', [Validators.required]],
+      fullPrice: [null, [Validators.required]],
+      pricePerSession: [null, [this.conditionallyRequiredValidator]],
+      currency: ['USD', [Validators.required]],
+      numSessions: [null, [Validators.required]],
+      duration: [null, [Validators.required]],
+      sellerUid: ['', [Validators.required]],
+      stripeId: ['', [Validators.required]],
+      coachName: ['', [Validators.required]],
+      coachPhoto: ['', [Validators.required]],
+    });
+  }
+
+  // https://medium.com/ngx/3-ways-to-implement-conditional-validation-of-reactive-forms-c59ed6fc3325
+  conditionallyRequiredValidator(formControl: AbstractControl) {
+    if (!formControl.parent) {
+      return null;
+    }
+
+    if (formControl.parent.get('pricingStrategy').value === 'flexible') {
+      return Validators.required(formControl);
+    }
+    return null;
+  }
+
+  importProgramData() {
+    this.programForm.patchValue({
+      programId: this.program.programId,
+      title: this.program.title,
+      subtitle: this.program.subtitle,
+      description: this.program.description,
+      language: this.program.language,
+      category: this.program.category,
+      level: this.program.level,
+      subject: this.program.subject,
+      pricingStrategy: this.program.pricingStrategy,
+      fullPrice: this.program.fullPrice,
+      pricePerSession: this.program.pricePerSession,
+      currency: this.program.currency,
+      numSessions: this.program.numSessions,
+      duration: this.program.duration,
+      coachName: this.program.coachName,
+      coachPhoto: this.program.coachPhoto,
+      stripeId: this.program.stripeId,
+      sellerUid: this.program.sellerUid
+    });
   }
 
   getDisplayDate(unix: number) {
@@ -36,70 +104,6 @@ export class ProgramSubmitComponent implements OnInit {
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const day = days[date.getDay()];
     return `${day} ${date.toLocaleDateString()}`;
-  }
-
-  isProgramValid(program: CoachingProgram) {
-    if (!program) {
-      return false;
-    }
-    if (program.reviewRequest && program.reviewRequest.status === 'submitted') {
-      return false;
-    }
-    if (program.reviewRequest && program.reviewRequest.status === 'in-review') {
-      return false;
-    }
-    if (program.reviewRequest && program.reviewRequest.status === 'approved') {
-      return false;
-    }
-    if (!program.programId) {
-      return false;
-    }
-    if (!program.title) {
-      return false;
-    }
-    if (!program.subtitle) {
-      return false;
-    }
-    if (!program.description) {
-      return false;
-    }
-    if (!program.language) {
-      return false;
-    }
-    if (!program.category) {
-      return false;
-    }
-    if (!program.level) {
-      return false;
-    }
-    if (!program.subject) {
-      return false;
-    }
-    if (!program.pricingStrategy) {
-      return false;
-    }
-    if (!program.currency) {
-      return false;
-    }
-    if (!program.fullPrice) {
-      return false;
-    }
-    if (program.pricingStrategy === 'flexible' && !program.pricePerSession) {
-      return false;
-    }
-    if (!program.sellerUid) {
-      return false;
-    }
-    if (!program.stripeId) {
-      return false;
-    }
-    if (!program.coachName) {
-      return false;
-    }
-    if (!program.coachPhoto) {
-      return false;
-    }
-    return true;
   }
 
   async onSubmit() {
@@ -113,7 +117,7 @@ export class ProgramSubmitComponent implements OnInit {
     }
 
     // check if program valid
-    if (!this.isProgramValid(this.program)) {
+    if (this.programForm.invalid) {
       this.alertService.alert('warning-message', 'Program invalid for review submission. Please contact support.');
       this.requesting = false;
       return;
