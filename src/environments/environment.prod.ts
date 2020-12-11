c3a59d0c419a8a229c04a6d7954ef327c358c9b7
Alex <alexandrit98226@gmail.com>
2020-12-11 12:42:11 +0200

    test keys for stripe edited'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,7 @@ export const environment = {
   appId: '1:765043503954:web:5e5b0b376f5a7bf349008b',
   measurementId: 'G-S4801FLL5M',
   algoliaApiKey: '73c827f1b21571be69a545f2728f087c',
-  algoliaApplicationID: 'PXC7SZHHT9'
+  algoliaApplicationID: 'PXC7SZHHT9',
+  stripeClientId: '&client_id=ca_Gl8J06rwgRmJRMoX00JSaSG9SKspTh9t',
+  stripeCourseKey: 'pk_live_GFTeJnPVGhgVifaASOsjEvXf00faFIpXu2'
 };
