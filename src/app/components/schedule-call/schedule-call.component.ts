163586a612fd163abcc66f4380e91fa3cc401c72
Greg Hamilton <greghamilton1@gmail.com>
2020-12-24 11:21:53 +0000

    added workaround for enabledDates bug

--- src/app/components/schedule-call/schedule-call.component.ts
+++ src/app/components/schedule-call/schedule-call.component.ts
@@ -29,20 +29,15 @@ export class ScheduleCallComponent implements OnInit {
   public browser: boolean;
   public userId: string;
   private subscriptions: Subscription = new Subscription();
-  public dayToSelect: Array<Date> = [];
+  public uniqueEnabledDays: Array<Date> = [];
   public timeToSelect: Array<Date> = [];
-  public availableEvents: CustomCalendarEvent[] | [];
+  public discoveryAvailableEvents: CustomCalendarEvent[] | [];
   public todayEvents: Array<any>;
 
   // ngx datePicker
   public bsInlineValue = new Date();
-  public dateSelectConfig = {
-    datesEnabled: [
-      new Date('2020-12-24'),
-      new Date('2020-12-25'),
-      new Date('2020-12-26'),
-    ]
-  } as BsDatepickerConfig;
+  public dateSelectConfig = { } as BsDatepickerConfig;
+  public disabledDates: Array<Date> = [];
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
@@ -73,25 +68,64 @@ export class ScheduleCallComponent implements OnInit {
   }
 
   getCoachCalendarEvents() {
+    // fetch this coach's discovery type events that are still available for booking
     this.subscriptions.add(
       this.dataService.getUserAvailableDiscoveryEvents(this.coachId).subscribe(next => {
         console.log(next);
         if (next) {
-          this.availableEvents = next;
-          this.dayToSelect = [];
-          this.availableEvents.forEach( i => {
+          this.discoveryAvailableEvents = next;
+
+          // calculate the unique days that have an available discovery event
+          this.uniqueEnabledDays = [];
+          this.discoveryAvailableEvents.forEach( i => {
             // @ts-ignore
             const startDate = new Date(i.start.seconds * 1000);
             const day = new Date(startDate.getUTCFullYear(), startDate.getUTCMonth(), startDate.getUTCDate());
-            if (!this.dayToSelect.some( (date) => this.isSameDay(date, day))) {
-              this.dayToSelect.push(day);
+            if (!this.uniqueEnabledDays.some( (date) => this.isSameDay(date, day))) {
+              this.uniqueEnabledDays.push(day);
             }
           });
           }
-        console.log('UNIQUE DAYS:', this.dayToSelect);
+        console.log('Unique enabled days:', this.uniqueEnabledDays);
+
+        // now we have the unique available days, calculate the dates to disable on the calendar
+        // Note: this is a required workaround because of a bug in ngx-bootstrap datePicker where
+        // datesEnabled is not working.
+        this.GetDisabledDates(this.uniqueEnabledDays);
       },
         error => console.log('error logger', error.message))
     );
+  }
+
+  GetDisabledDates(enabledDates: Array<Date>) {
+    /*
+      Required workaround to reverse datesEnabled. Pass in an array of dates to enable and this method
+      will populate an array of dates to disable.
+      Disables every date between a defined start and end point that is not in the enabled array.
+    */
+    const millisecondPerDay = 24 * 60 * 60 * 1000;
+    const now = new Date();
+    let startDate: Date = new Date(now.setFullYear(now.getFullYear() - 1));
+    const endDate: Date = new Date(now.setFullYear(now.getFullYear() + 2)); // change as per need
+    console.log('get disabled dates start:', startDate);
+    console.log('get disabled dates end:', endDate);
+    this.disabledDates = [];
+    do {
+      let found = false;
+      // tslint:disable-next-line: prefer-for-of
+      for (let i = 0; i < enabledDates.length; i++) {
+        const excludeDate: Date = enabledDates[i];
+        if (this.isSameDay(excludeDate, startDate)) {
+          found = true;
+        }
+      }
+      if (!found) {
+        this.disabledDates.push(startDate);
+      }
+      startDate = new Date((startDate.getTime() + millisecondPerDay));
+    } while (startDate <= endDate);
+    console.log('Calculated disabled dates number: ' + this.disabledDates.length);
+    // console.log('Calculated disabled dates: ', this.disabledDates);
   }
 
   daySelect(event: any) {
