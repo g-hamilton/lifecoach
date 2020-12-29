e1409e1a6fd4b19ebbeddce02547ba43c0829828
Alex <alexandrit98226@gmail.com>
2020-12-29 14:26:19 +0200

    resized test images

---
+++ src/app/interfaces/image-path.interface.ts
@@ -0,0 +1,31 @@
+export interface ProgramImagePaths {
+  original: {
+    575: string;
+    768: string;
+    991: string;
+    1200: string;
+    fullSize: string;
+  };
+  webp: {
+    575: string;
+    768: string;
+    991: string;
+    1200: string;
+    fullSize: string;
+  };
+  other?: any;
+}
+
+export interface UserAvatarImagePaths {
+  original: {
+    124: string;
+    248: string;
+    fullSize: string;
+  };
+  webp: {
+    124: string;
+    248: string;
+    fullSize: string;
+  };
+  other?: any;
+}
