ba97e46a1ea1ab701164c2df84f7b7ec8f7c74ac
Greg Hamilton <greghamilton1@gmail.com>
2021-01-01 14:35:38 +0000

    time fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2450,8 +2450,8 @@ exports.orderCoachSession = functions
       name: 'booked_coach_session',
       properties: {
         type: event.type,
-        start: new Date(event.start.seconds).toUTCString(),
-        end: new Date(event.end.seconds).toUTCString(),
+        start: new Date(event.start.seconds * 1000).toUTCString(),
+        end: new Date(event.end.seconds * 1000).toUTCString(),
         coach_name: `${coachProfile ? coachProfile.firstName : 'Lifecoach'} ${coachProfile ? coachProfile.lastName : 'Coach'}`,
         coach_photo: `${coachProfile ? coachProfile.photo : 'https://eu.ui-avatars.com/api/?name=lifecoach+coach&background=00f2c3&color=fff&rounded=true&bold=true'}`,
         landing_url: `https://lifecoach.io/my-sessions/${sessionId}`
@@ -2465,8 +2465,8 @@ exports.orderCoachSession = functions
       name: 'session_booked',
       properties: {
         type: event.type,
-        start: new Date(event.start.seconds).toUTCString(),
-        end: new Date(event.end.seconds).toUTCString(),
+        start: new Date(event.start.seconds * 1000).toUTCString(),
+        end: new Date(event.end.seconds * 1000).toUTCString(),
         user_name: userName,
         user_photo: userPhoto,
         landing_url: `https://lifecoach.io/my-sessions/${sessionId}`
@@ -2583,8 +2583,8 @@ exports.cancelCoachSession = functions
         name: 'coach_cancelled_your_session',
         properties: {
           type: coachCalEvent.type,
-          start: new Date(coachCalEvent.start.seconds).toUTCString(),
-          end: new Date(coachCalEvent.end.seconds).toUTCString(),
+          start: new Date(coachCalEvent.start.seconds * 1000).toUTCString(),
+          end: new Date(coachCalEvent.end.seconds * 1000).toUTCString(),
           coach_name: `${coachProfile ? coachProfile.firstName : 'Lifecoach'} ${coachProfile ? coachProfile.lastName : 'Coach'}`,
           coach_photo: `${coachProfile ? coachProfile.photo : 'https://eu.ui-avatars.com/api/?name=lifecoach+coach&background=00f2c3&color=fff&rounded=true&bold=true'}`,
           landing_url: `https://lifecoach.io/my-sessions/${coachCalEvent.sessionId}`
@@ -2598,8 +2598,8 @@ exports.cancelCoachSession = functions
         name: 'coach_cancelled_own_session',
         properties: {
           type: coachCalEvent.type,
-          start: new Date(coachCalEvent.start.seconds).toUTCString(),
-          end: new Date(coachCalEvent.end.seconds).toUTCString(),
+          start: new Date(coachCalEvent.start.seconds * 1000).toUTCString(),
+          end: new Date(coachCalEvent.end.seconds * 1000).toUTCString(),
           user_name: coachCalEvent.orderedByName,
           user_photo: coachCalEvent.orderedByPhoto,
           landing_url: `https://lifecoach.io/my-sessions/${coachCalEvent.sessionId}`
@@ -2620,8 +2620,8 @@ exports.cancelCoachSession = functions
         name: 'you_cancelled_coach_session',
         properties: {
           type: coachCalEvent.type,
-          start: new Date(coachCalEvent.start.seconds).toUTCString(),
-          end: new Date(coachCalEvent.end.seconds).toUTCString(),
+          start: new Date(coachCalEvent.start.seconds * 1000).toUTCString(),
+          end: new Date(coachCalEvent.end.seconds * 1000).toUTCString(),
           coach_name: `${coachProfile ? coachProfile.firstName : 'Lifecoach'} ${coachProfile ? coachProfile.lastName : 'Coach'}`,
           coach_photo: `${coachProfile ? coachProfile.photo : 'https://eu.ui-avatars.com/api/?name=lifecoach+coach&background=00f2c3&color=fff&rounded=true&bold=true'}`,
           landing_url: `https://lifecoach.io/my-sessions/${coachCalEvent.sessionId}`
@@ -2635,8 +2635,8 @@ exports.cancelCoachSession = functions
         name: 'user_cancelled_your_session',
         properties: {
           type: coachCalEvent.type,
-          start: new Date(coachCalEvent.start.seconds).toUTCString(),
-          end: new Date(coachCalEvent.end.seconds).toUTCString(),
+          start: new Date(coachCalEvent.start.seconds * 1000).toUTCString(),
+          end: new Date(coachCalEvent.end.seconds * 1000).toUTCString(),
           user_name: coachCalEvent.orderedByName,
           user_photo: coachCalEvent.orderedByPhoto,
           landing_url: `https://lifecoach.io/my-sessions/${coachCalEvent.sessionId}`
