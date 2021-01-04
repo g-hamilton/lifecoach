712798734b4fc220b81b010d44e8595870b6ff99
Greg Hamilton <greghamilton1@gmail.com>
2021-01-04 10:12:20 +0000

    loading enrolled in programs

--- src/app/pages/person-history/person.history.component.ts
+++ src/app/pages/person-history/person.history.component.ts
@@ -4,9 +4,11 @@ import { AuthService } from 'app/services/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { CRMPerson } from 'app/interfaces/crm.person.interface';
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { CrmPeopleService } from 'app/services/crm-people.service';
 import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
 import { CoachInviteComponent } from 'app/components/coach-invite/coach-invite.component';
+import { DataService } from 'app/services/data.service';
 
 @Component({
   selector: 'app-person-history',
@@ -22,13 +24,16 @@ export class PersonHistoryComponent implements OnInit, OnDestroy {
   public person: CRMPerson;
   private subscriptions: Subscription = new Subscription();
   public msgUrl = '/messages';
+  public enrolledInCourses = [];
+  public enrolledInPrograms = [];
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
     private authService: AuthService,
     private crmPeopleService: CrmPeopleService,
     private route: ActivatedRoute,
-    private modalService: BsModalService
+    private modalService: BsModalService,
+    private dataService: DataService
   ) {
   }
 
@@ -57,11 +62,21 @@ export class PersonHistoryComponent implements OnInit, OnDestroy {
 
   loadPerson() {
     this.subscriptions.add(
-      this.crmPeopleService.getUserPerson(this.userId, this.personId).subscribe(async p => {
-        if (p) {
-          console.log('person:', p);
-          this.person = await this.crmPeopleService.getFilledPerson(this.userId, p, this.personId);
+      this.crmPeopleService.getUserPerson(this.userId, this.personId).subscribe(async person => {
+        if (person) {
+          console.log('person:', person);
+          this.person = await this.crmPeopleService.getFilledPerson(this.userId, person, this.personId);
           this.updateMsgUrl();
+
+          // work out which eCourses and programs this person has enrolled in by looking at their history array
+          if (this.person.history) {
+            // deal with programs
+            const programs = this.person.history.filter(i => i.action === 'enrolled_in_program_session' || i.action === 'enrolled_in_full_program');
+            const mapped = programs.map(i => i.programId); // make an array of program ids (may contain duplicates if paying per session!)
+            const uniqueProgramIds = [...new Set(mapped)]; // remove any duplicates
+            this.enrolledInPrograms = []; // reset
+            uniqueProgramIds.forEach(i => this.loadProgram(i));
+          }
         }
       })
     );
@@ -93,6 +108,20 @@ export class PersonHistoryComponent implements OnInit, OnDestroy {
       }
     };
     this.bsModalRef = this.modalService.show(CoachInviteComponent, config);
+  }
+
+  loadProgram(programId: string) {
+    // fetch the program and push it to the array of programs the person is enrolled in
+    this.subscriptions.add(
+      this.dataService.getPublicProgram(programId)
+      .pipe(take(1))
+      .subscribe(program => {
+        if (program) {
+          this.enrolledInPrograms.push(program);
+          console.log('enrolled in programs:', this.enrolledInPrograms);
+        }
+      })
+    );
   }
 
   ngOnDestroy() {
