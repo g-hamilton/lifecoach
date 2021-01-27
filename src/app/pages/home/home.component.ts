28e7c7ec1d1589697872440e5344438335885c6d
Greg Hamilton <greghamilton1@gmail.com>
2021-01-27 09:13:42 +0000

    working on landing page update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, PLATFORM_ID, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, OnDestroy } from '@angular/core';
 import { Title, Meta, TransferState, makeStateKey } from '@angular/platform-browser';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
@@ -15,7 +15,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy {
   public browser: boolean;
 
   public newestCoaches: AlgoliaCoachProfile[];
@@ -25,11 +25,11 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   public clientCurrency: string;
   public clientCountry: string;
   public rates: any;
-  public timer: any;
-  public wordIndex = 0;
-  public words = ['Life', 'Academic', 'Business', 'Career', 'Family', 'Financial', 'Fitness', 'Health', 'Holistic', 'Management', 'Mindset', 'Parenting', 'Productivity', 'Relationship', 'Relocation', 'Retirement', 'Spiritual', 'Sports', 'Transformation', 'Wellness'];
 
   private subscriptions: Subscription = new Subscription();
+
+  public goals = ['Life', 'Academic', 'Business', 'Career', 'Family', 'Financial', 'Fitness', 'Health', 'Holistic', 'Management', 'Mindset', 'Parenting', 'Productivity', 'Relationship', 'Relocation', 'Retirement', 'Spiritual', 'Sports', 'Transformation', 'Wellness'];
+  public tagsToHighlight = [0, 3, 7, 10];
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
@@ -43,25 +43,35 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.titleService.setTitle('Lifecoach | Connect With Professional Coaches');
-    this.metaTagService.updateTag({name: 'description', content: 'Find professional coaches for every area of life with Lifecoach.io'});
+    this.titleService.setTitle('Lifecoach | The Premier Personal Coaching & Transformation Platform');
+    this.metaTagService.updateTag({name: 'description', content: 'Find professional coaching for every area of life with Lifecoach.io'});
 
     // Register a page view if we're in the browser (not SSR)
     if (isPlatformBrowser(this.platformId)) {
       this.browser = true;
       this.analyticsService.pageView();
-
-      // Check for saved client currency & country preference
-      const savedClientCurrencyPref = localStorage.getItem('client-currency');
-      const savedClientCountryPref = localStorage.getItem('client-country');
-      if (savedClientCurrencyPref && savedClientCountryPref) {
-        this.clientCurrency = savedClientCurrencyPref;
-        this.clientCountry = savedClientCountryPref;
-      } else {
-        this.getClientCurrencyAndCountryFromIP();
-      }
+      // this.checkSavedClientCurrencyAndCountry();
     }
 
+    // this.monitorPlatformRates();
+    // this.fetchNewestCoaches();
+    // this.fetchNewestCourses();
+
+  }
+
+  checkSavedClientCurrencyAndCountry() {
+    // Check for saved client currency & country preference
+    const savedClientCurrencyPref = localStorage.getItem('client-currency');
+    const savedClientCountryPref = localStorage.getItem('client-country');
+    if (savedClientCurrencyPref && savedClientCountryPref) {
+      this.clientCurrency = savedClientCurrencyPref;
+      this.clientCountry = savedClientCountryPref;
+    } else {
+      this.getClientCurrencyAndCountryFromIP();
+    }
+  }
+
+  monitorPlatformRates() {
     // Monitor platform rates for realtime price calculations
     this.subscriptions.add(
       this.dataService.getPlatformRates().subscribe(rates => {
@@ -71,7 +81,9 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
         }
       })
     );
+  }
 
+  fetchNewestCoaches() {
     // Fetch newest coaches data, checking state to avoid duplicate calls
     const STATE_KEY_COACHES = makeStateKey<any>('newest_coaches'); // create a key for saving/retrieving a state
     const StateDataCoaches = this.transferState.get(STATE_KEY_COACHES, null as any); // checking if data in the storage exists
@@ -86,7 +98,9 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       this.newestCoaches = StateDataCoaches;
       this.transferState.remove(STATE_KEY_COACHES);
     }
+  }
 
+  fetchNewestCourses() {
     // Fetch newest courses data, checking state to avoid duplicate calls
     const STATE_KEY_COURSES = makeStateKey<any>('newest_courses'); // create a key for saving/retrieving a state
     const StateDataCourses = this.transferState.get(STATE_KEY_COURSES, null as any); // checking if data in the storage exists
@@ -100,20 +114,6 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     } else { // if data state exists retrieve it from the state storage
       this.newestCourses = StateDataCourses;
       this.transferState.remove(STATE_KEY_COURSES);
-    }
-
-  }
-
-  ngAfterViewInit(): void {
-    if (this.browser) {
-      this.timer = setInterval(() => {
-        const tempIndex = this.wordIndex + 1;
-        if (this.words[tempIndex] != null) {
-          return this.wordIndex++;
-        } else {
-          return this.wordIndex = 0;
-        }
-      }, 2000);
     }
   }
 
@@ -148,9 +148,5 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
-
-    if (this.timer != null) {
-      clearInterval(this.timer);
-    }
   }
 }
