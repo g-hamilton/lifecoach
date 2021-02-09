e6fa218aff6a61245c17be8408aa1e6f68856b03
Greg Hamilton <greghamilton1@gmail.com>
2021-02-09 14:48:40 +0000

    logging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3291,6 +3291,7 @@ exports.onWriteUserProfileNode = functions
   }
   if (profile) {
     // Record has been updated
+    console.log('Profile data exists. Updating profile...');
 
     // *** TEMP TEMP TEMP ***
 
@@ -3347,7 +3348,7 @@ exports.onWriteUserProfileNode = functions
 
     if (profile.isPublic) {
       // Profile is set to public. Sync with public node.
-      // console.log('User profile is marked public. Syncing with public data...')
+      console.log('User profile is marked public. Syncing with public data...')
       return db.collection(`public-coaches`)
       .doc(userId)
       .set(profile, {merge: true})
