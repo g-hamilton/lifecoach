2e7136ee812d9059b7b195c07acfcd2bfe769a2d
Greg Hamilton <greghamilton1@gmail.com>
2021-02-14 11:07:14 +0000

    templates if payout enabled or not

--- src/app/pages/partner-link/partner.link.component.ts
+++ src/app/pages/partner-link/partner.link.component.ts
@@ -3,6 +3,8 @@ import { isPlatformBrowser } from '@angular/common';
 import { AuthService } from 'app/services/auth.service';
 import { AnalyticsService } from 'app/services/analytics.service';
 import { Subscription } from 'rxjs';
+import { UserAccount } from 'app/interfaces/user.account.interface';
+import { DataService } from 'app/services/data.service';
 
 @Component({
   selector: 'app-partner-link',
@@ -13,11 +15,13 @@ export class PartnerLinkComponent implements OnInit, OnDestroy {
 
   public browser: boolean;
   private userId: string;
+  public account: UserAccount;
   private subscriptions: Subscription = new Subscription();
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
     private authService: AuthService,
+    private dataService: DataService,
     private analyticsService: AnalyticsService
   ) {
   }
@@ -34,9 +38,22 @@ export class PartnerLinkComponent implements OnInit, OnDestroy {
       this.authService.getAuthUser().subscribe(user => {
         if (user) {
           this.userId = user.uid;
+          this.checkPayoutsEnabled();
         }
       })
     );
+  }
+
+  checkPayoutsEnabled() {
+    this.subscriptions.add(
+        this.dataService.getUserAccount(this.userId).subscribe(userAccount => {
+            if (userAccount) {
+                this.account = userAccount;
+                console.log('User Account:', this.account);
+            }
+          }
+        )
+      );
   }
 
   ngOnDestroy() {
