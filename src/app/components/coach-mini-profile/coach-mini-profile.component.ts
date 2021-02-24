9d40995469fe6786dfa470c65e167539a5dc66b3
Greg Hamilton <greghamilton1@gmail.com>
2021-02-24 14:27:01 +0000

    added stripe package. updated mini profile

--- src/app/components/coach-mini-profile/coach-mini-profile.component.ts
+++ src/app/components/coach-mini-profile/coach-mini-profile.component.ts
@@ -3,13 +3,10 @@ import { CoachingCourse } from 'app/interfaces/course.interface';
 import { isPlatformServer } from '@angular/common';
 import { makeStateKey, TransferState } from '@angular/platform-browser';
 import { Observable, Subscription } from 'rxjs';
-import { CourseReview } from 'app/interfaces/course-review';
-import { ProgramReview } from 'app/interfaces/program-review';
 import { DataService } from 'app/services/data.service';
 import { CoachingProgram } from 'app/interfaces/coach.program.interface';
 import { CoachProfile } from 'app/interfaces/coach.profile.interface';
 import { ReviewsService } from 'app/services/reviews.service';
-import { ServiceReview } from 'app/interfaces/service.review.interface';
 import { CoachingService } from 'app/interfaces/coaching.service.interface';
 
 
@@ -26,15 +23,6 @@ export class CoachMiniProfileComponent implements OnInit, OnChanges, OnDestroy {
 
   private subscriptions: Subscription = new Subscription();
 
-  public courseReviews: CourseReview[];
-  public programReviews: ProgramReview[];
-  public serviceReviews: ServiceReview[];
-  public avgCourseRating: number;
-  public avgProgramRating: number;
-  public avgServiceRating: number;
-  public sellerCourseEnrollments: number;
-  public sellerProgramEnrollments: number;
-  public sellerServiceEnrollments: number;
   public sellerCourses: Observable<CoachingCourse[]>;
   public sellerPrograms: Observable<CoachingProgram[]>;
   public sellerServices: Observable<CoachingService[]>;
@@ -52,15 +40,11 @@ export class CoachMiniProfileComponent implements OnInit, OnChanges, OnDestroy {
   ngOnChanges() {
     if (this.coachId) {
       this.fetchCoachProfile();
-      this.fetchCourseReviewsData();
-      this.fetchProgramReviewsData();
-      this.fetchServiceReviewsData();
-      // this.fetchSellerCourseEnrollmentsData();
-      // this.fetchSellerProgramEnrollmentsData();
-      // this.fetchSellerServiceEnrollmentsData();
       this.fetchSellerCoursesData();
       this.fetchSellerProgramsData();
       this.fetchSellerServicesData();
+      this.fetchCoachTestimonials();
+      this.fetchCoachTotalClients();
     }
   }
 
@@ -88,147 +72,6 @@ export class CoachMiniProfileComponent implements OnInit, OnChanges, OnDestroy {
       this.transferState.remove(COACH_KEY);
     }
   }
-
-  fetchCourseReviewsData() {
-    const REVIEWS_KEY = makeStateKey<any>('course-reviews'); // create a key for saving/retrieving state
-
-    const reviewsData = this.transferState.get(REVIEWS_KEY, null as any); // checking if data in the storage exists
-
-    if (reviewsData === null) { // if state data does not exist - retrieve it from the api
-
-      // get this coach's course reviews
-      this.subscriptions.add(
-        this.reviewsService.getSellerCourseReviews(this.coachId).subscribe(data => {
-          this.courseReviews = data;
-
-          // calc avg rating for this coach's courses
-          const ratings = this.courseReviews.map(v => v.starValue);
-          this.avgCourseRating = ratings.length ? ratings.reduce((total, val) => total + val) / ratings.length : null;
-
-          if (isPlatformServer(this.platformId)) { // if we're server side, store the retrieved data as a state
-            this.transferState.set(REVIEWS_KEY, this.courseReviews);
-          }
-        })
-      );
-
-    } else { // if reviews state data exists retrieve it from the state storage
-      this.courseReviews = reviewsData;
-      // console.log('Course reviews data for user:', reviewsData);
-      this.transferState.remove(REVIEWS_KEY);
-    }
-  }
-
-  fetchProgramReviewsData() {
-    const REVIEWS_KEY = makeStateKey<any>('program-reviews'); // create a key for saving/retrieving state
-
-    const reviewsData = this.transferState.get(REVIEWS_KEY, null as any); // checking if data in the storage exists
-
-    if (reviewsData === null) { // if state data does not exist - retrieve it from the api
-
-      // get this coach's program reviews
-      this.subscriptions.add(
-        this.reviewsService.getSellerProgramReviews(this.coachId).subscribe(data => {
-          this.programReviews = data;
-
-          // calc avg rating for this coach's programs
-          const ratings = this.programReviews.map(v => v.starValue);
-          this.avgProgramRating = ratings.length ? ratings.reduce((total, val) => total + val) / ratings.length : null;
-
-          if (isPlatformServer(this.platformId)) { // if we're server side, store the retrieved data as a state
-            this.transferState.set(REVIEWS_KEY, this.programReviews);
-          }
-        })
-      );
-
-    } else { // if reviews state data exists retrieve it from the state storage
-      this.programReviews = reviewsData;
-      // console.log('Program reviews data for user:', reviewsData);
-      this.transferState.remove(REVIEWS_KEY);
-    }
-  }
-
-  fetchServiceReviewsData() {
-    const REVIEWS_KEY = makeStateKey<any>('service-reviews'); // create a key for saving/retrieving state
-
-    const reviewsData = this.transferState.get(REVIEWS_KEY, null as any); // checking if data in the storage exists
-
-    if (reviewsData === null) { // if state data does not exist - retrieve it from the api
-
-      // get this coach's service reviews
-      this.subscriptions.add(
-        this.reviewsService.getSellerServiceReviews(this.coachId).subscribe(data => {
-          this.serviceReviews = data;
-
-          // calc avg rating for this coach's services
-          const ratings = this.serviceReviews.map(v => v.starValue);
-          this.avgServiceRating = ratings.length ? ratings.reduce((total, val) => total + val) / ratings.length : null;
-
-          if (isPlatformServer(this.platformId)) { // if we're server side, store the retrieved data as a state
-            this.transferState.set(REVIEWS_KEY, this.serviceReviews);
-          }
-        })
-      );
-
-    } else { // if reviews state data exists retrieve it from the state storage
-      this.programReviews = reviewsData;
-      // console.log('Program reviews data for user:', reviewsData);
-      this.transferState.remove(REVIEWS_KEY);
-    }
-  }
-
-  // async fetchSellerCourseEnrollmentsData() {
-  //   const ENROLLMENTS_KEY = makeStateKey<any>('course-enrollments'); // create a key for saving/retrieving state
-
-  //   const enrollmentsData = this.transferState.get(ENROLLMENTS_KEY, null as any); // checking if data in the storage exists
-
-  //   if (enrollmentsData === null) { // if state data does not exist - retrieve it from the api
-  //     this.sellerCourseEnrollments = await this.dataService.getTotalPublicEnrollmentsByCourseSeller(this.coachId);
-
-  //     if (isPlatformServer(this.platformId)) { // if we're server side, store the retrieved data as a state
-  //       this.transferState.set(ENROLLMENTS_KEY, this.sellerCourseEnrollments);
-  //     }
-
-  //   } else { // if state data exists retrieve it from the state storage
-  //     this.sellerCourseEnrollments = enrollmentsData;
-  //     this.transferState.remove(ENROLLMENTS_KEY);
-  //   }
-  // }
-
-  // async fetchSellerProgramEnrollmentsData() {
-  //   const ENROLLMENTS_KEY = makeStateKey<any>('program-enrollments'); // create a key for saving/retrieving state
-
-  //   const enrollmentsData = this.transferState.get(ENROLLMENTS_KEY, null as any); // checking if data in the storage exists
-
-  //   if (enrollmentsData === null) { // if state data does not exist - retrieve it from the api
-  //     this.sellerProgramEnrollments = await this.dataService.getTotalPublicProgramEnrollmentsBySeller(this.coachId);
-
-  //     if (isPlatformServer(this.platformId)) { // if we're server side, store the retrieved data as a state
-  //       this.transferState.set(ENROLLMENTS_KEY, this.sellerProgramEnrollments);
-  //     }
-
-  //   } else { // if state data exists retrieve it from the state storage
-  //     this.sellerProgramEnrollments = enrollmentsData;
-  //     this.transferState.remove(ENROLLMENTS_KEY);
-  //   }
-  // }
-
-  // async fetchSellerServiceEnrollmentsData() {
-  //   const ENROLLMENTS_KEY = makeStateKey<any>('service-enrollments'); // create a key for saving/retrieving state
-
-  //   const enrollmentsData = this.transferState.get(ENROLLMENTS_KEY, null as any); // checking if data in the storage exists
-
-  //   if (enrollmentsData === null) { // if state data does not exist - retrieve it from the api
-  //     this.sellerServiceEnrollments = await this.dataService.getTotalPublicServiceEnrollmentsBySeller(this.coachId);
-
-  //     if (isPlatformServer(this.platformId)) { // if we're server side, store the retrieved data as a state
-  //       this.transferState.set(ENROLLMENTS_KEY, this.sellerServiceEnrollments);
-  //     }
-
-  //   } else { // if state data exists retrieve it from the state storage
-  //     this.sellerServiceEnrollments = enrollmentsData;
-  //     this.transferState.remove(ENROLLMENTS_KEY);
-  //   }
-  // }
 
   fetchSellerCoursesData() {
     const COURSES_KEY = makeStateKey<any>('courses'); // create a key for saving/retrieving state
@@ -301,6 +144,43 @@ export class CoachMiniProfileComponent implements OnInit, OnChanges, OnDestroy {
       this.transferState.remove(SERVICES_KEY);
     }
   }
+
+  fetchCoachTestimonials() {
+    // todo!
+  }
+
+  fetchCoachTotalClients() {
+    // todo!
+  }
+
+  // fetchCourseReviewsData() {
+  //   const REVIEWS_KEY = makeStateKey<any>('course-reviews'); // create a key for saving/retrieving state
+
+  //   const reviewsData = this.transferState.get(REVIEWS_KEY, null as any); // checking if data in the storage exists
+
+  //   if (reviewsData === null) { // if state data does not exist - retrieve it from the api
+
+  //     // get this coach's course reviews
+  //     this.subscriptions.add(
+  //       this.reviewsService.getSellerCourseReviews(this.coachId).subscribe(data => {
+  //         this.courseReviews = data;
+
+  //         // calc avg rating for this coach's courses
+  //         const ratings = this.courseReviews.map(v => v.starValue);
+  //         this.avgCourseRating = ratings.length ? ratings.reduce((total, val) => total + val) / ratings.length : null;
+
+  //         if (isPlatformServer(this.platformId)) { // if we're server side, store the retrieved data as a state
+  //           this.transferState.set(REVIEWS_KEY, this.courseReviews);
+  //         }
+  //       })
+  //     );
+
+  //   } else { // if reviews state data exists retrieve it from the state storage
+  //     this.courseReviews = reviewsData;
+  //     // console.log('Course reviews data for user:', reviewsData);
+  //     this.transferState.remove(REVIEWS_KEY);
+  //   }
+  // }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
